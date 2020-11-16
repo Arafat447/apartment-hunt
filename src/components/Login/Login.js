06fd71265fdfcdfe45968b0bb9e277880220156b
Arafat447 <tahmidhasan184@gmail.com>
2020-11-16 21:53:15 +0600

    form updated

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,40 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
+import { useForm } from "react-hook-form";
 import './Login.scss'
 
 const Login = () => {
-    const [newUser,setNewUser] = useState(false)
+    const [newUser, setNewUser] = useState(false);
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div className="login-wrapper">
             <Header></Header>
             <div className="login-container container mt-5 ">
-                <div className="from-group form-container bg-light login-form mx-auto p-3">
-                    {!newUser &&<h2 className="mb-3" >Log in</h2>}
-                    {newUser &&<h2 className="mb-3" >Create an account</h2>}
-                    <form className="mt-5">
-                      { newUser && <div className="form-group">
-                            <input name="first-name"  type="text" className="form-control" id=""  required placeholder="Enter your first name" />
+                <div className="from-group form-container login-form mx-auto p-3">
+                    {!newUser && <h2 className="mb-3" >Log in</h2>}
+                    {newUser && <h2 className="mb-3" >Create an account</h2>}
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        {newUser && <div className="form-group">
+                            <input name="firstName" ref={register({ required: true })} type="text" className="form-control" id="" placeholder="Enter your first name" />
+                            {errors.firstName && <span>This field is required</span>}
                         </div>}
-                       { newUser && <div className="form-group">
-                            <input name="last-name"  type="text" className="form-control" id=""  required placeholder="Enter your last name" />
+                        {newUser && <div className="form-group">
+                            <input name="lastName" ref={register({ required: true })} type="text" className="form-control" id="" placeholder="Enter your last name" />
+                            {errors.lastName && <span>This field is required</span>}
                         </div>}
                         <div className="form-group">
-                            <input name="email"  type="email" className="form-control" id=""  required placeholder="Enter email" />
+                            <input name="email" ref={register({ required: true })} type="text" className="form-control" id="" placeholder="Enter your email" />
+                            {errors.email && <span>This field is required</span>}
                         </div>
-                        <div className="form-group">
-                            <input name="password" type="password" className="form-control" id=""  required placeholder="Password" />
+                         <div className="form-group">
+                            <input name="password" ref={register({ required: true })} type="password" className="form-control" id="" placeholder="Password" />
+                            {errors.password && <span>This field is required</span>}
                         </div>
                         {newUser && <div className="form-group">
-                            <input name="confirm-password" type="password" className="form-control" id=""  required placeholder="Confirm Password" />
+                            <input name="password" ref={register({ required: true })} type="password" className="form-control" id="" placeholder="Confirm password" />
+                            {errors.password && <span>This field is required</span>}
                         </div>}
                         {!newUser &&<div className="d-flex justify-content-between my-2" >
                             <div className="form-check">
@@ -37,19 +45,20 @@ const Login = () => {
                                 <a href="/" className="login-link">Forget  password</a>
                             </div>
                         </div>}
-                        {!newUser &&<button type="submit" className="btn btn-custom text-white  mt-3 d-block w-75 mx-auto ">Login</button>}
-                        {newUser &&<button type="submit" className="btn btn-custom text-white  mt-3 d-block w-75 mx-auto ">Create an account</button>}
+                        {!newUser &&<input type="submit" value="Login" className="btn btn-custom text-white  mt-3 d-block w-75 mx-auto "/>}
+                        {newUser &&<input type="submit" value="Create an account" className="btn btn-custom text-white  mt-3 d-block w-75 mx-auto "/>}
                     </form>
-                    {!newUser &&<div className="d-flex justify-content-center mt-3" >
+
+                    {!newUser && <div className="d-flex justify-content-center mt-3" >
                         <p>Do not have account  ? </p>
                         <Link onClick={() => setNewUser(!newUser)} className="ml-3 login-link">Create an account</Link>
                     </div>}
-                    {newUser &&<div className="d-flex justify-content-center mt-3" >
+                    {newUser && <div className="d-flex justify-content-center mt-3" >
                         <p>Already have an account  ? </p>
                         <Link onClick={() => setNewUser(!newUser)} className="ml-3 login-link">Login</Link>
                     </div>}
                 </div>
-                <div className="or-line d-flex justify-content-center" > <hr className="ml-auto"/> Or <hr className="mr-auto"/></div>
+                <div className="or-line d-flex justify-content-center" > <hr className="ml-auto" /> Or <hr className="mr-auto" /></div>
                 <button className="text-left d-flex sign-btn mx-auto align-items-center">
                     <img className="m-1" src="https://i.imgur.com/hPkbIgr.png" alt="" />
                     <h6 className="m-2 mx-5" >Continue with facebook </h6>
